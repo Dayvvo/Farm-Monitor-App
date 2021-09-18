809449f8ee6701770aa10d2e051d73b1e8e61792
Dayvvo <dyvvoo@gmail.com>
2021-09-18 22:59:35 +0100

    Commit

--- js/query.js
+++ js/query.js
@@ -6,3 +6,23 @@ $(document).ready(function(){
     });
     
     });
+
+
+
+let tabHeader = document.querySelector(".mappas");
+let tabIndicator = document.getElementsByClassName("tab-indicator")[0];
+let tabBody = document.getElementsByClassName("tab-body")[0];
+ 
+let tabsPane = tabHeader.getElementsByTagName("a");
+ 
+for(let i=0;i<tabsPane.length;i++){
+  tabsPane[i].addEventListener("click",function(e){
+    e.preventDefault();
+    tabHeader.getElementsByClassName("active")[0].classList.remove("active");
+    tabsPane[i].classList.add("active");
+    tabBody.querySelector(".active").classList.remove("active");
+    tabBody.querySelectorAll(".statbox")[i].classList.add("active");
+    
+    // tabIndicator.style.left = `calc(calc(100% / 4) * ${i})`;
+  });
+}
